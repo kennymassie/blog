56ed2adf015746aeb3d8530a689922c2381021cf
Kenny Massie <NA>
2023-02-06 16:22:42 -0500

    Deployment configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
+
 const nextConfig = {
   reactStrictMode: true,
+  /**
+   * Tell Next.js where the `public` folder is.
+   * Replace `nextjs-github-pages` with your Github repo project name.
+   */
+  assetPrefix: isProd ? "/blog/" : "",
+  /**
+   * Disable server-based image optimization.
+   *
+   * @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable
+   */
+  images: {
+    unoptimized: true,
+    // loader: unsplashed
+    // path:
+  },
 }
 
 module.exports = nextConfig
